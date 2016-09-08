ea660d1713a321427741bd27fc4214e045647f7b
conradhopp <conradehopp@gmail.com>
2016-09-08 14:39:05 -0400

    Update web_scraper.R

--- web_scraper.R
+++ web_scraper.R
@@ -7,30 +7,7 @@ library("dplyr")
 library("rvest")
 library("leaflet")
 
-urls <- c("http://www.myagentabby.com",
-          "http://www.alice-white.com/",
-          "http://www.billthorp.com/",
-          "http://www.insurewithdianne.com",
-          "http://www.doriswatson.com/",
-          "http://www.drushort.net",
-          "https://www.ejleinen.com/",
-          "http://www.emilyswift.com",
-          "https://www.callginam.com/",
-          "http://www.gregmorrisagency.com/",
-          "http://www.autoinsurela.com/",
-          "http://www.jim-demaio.com/",
-          "http://www.jimmyurban.com",
-          "http://www.joebonow.com/",
-          "http://www.jlanginsurance.com",
-          "http://www.insureherndon.com",
-          "http://www.sfphilly.com",
-          "http://www.inlandempiresf.com",
-          "https://www.nikkinamy.com",
-          "http://www.pamelapatterson.com/",
-          "http://www.robdegeorge.com",
-          "http://stevealleninsurance.com",
-          "http://www.tammydobrotin.com",
-          "http://www.tonyedwards.net/")
+urls <- c("url 1", "url 2",...)
       
 df <- lapply(urls, function(u) {  
           
@@ -58,14 +35,12 @@ df2 <- lapply(urls, function(u) {
     
   })
 
-
 df2 <- do.call(rbind,df2)
 
 df3 <- rbind(df,df2)
 
-
 View(df3)
 
-write.csv(df3,"R/statefarmtest1.csv")
+write.csv(df3,"R/scrapedata.csv")
 
-write.table(df,"Rstatefarm.txt")
+write.table(df,"scrapedata.txt")
